5c970ad6f7497e39488ea54e9fc75c5f784f2f85
mahdiesad <mahdiyeh.sadeghian@gmail.com>
2017-07-16 16:02:25 +0430

    init commit

---
+++ app/Http/Controllers/testController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 7/16/2017
+ * Time: 3:03 PM
+ */
+
+namespace App\Http\Controllers;
+
+
+class testController extends Controller
+{
+    public function test(){
+
+        return view('mahdieh');
+
+    }
+}
